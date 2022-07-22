2cea118c6dd43ffa65bddce8c71cf8ce307042d7
webners1 <faizan_babar70@yahoo.com>
2022-07-23 01:32:31 +0500

    hash

---
+++ src/config.js
@@ -0,0 +1,196 @@
+const basePath = process.cwd();
+const { MODE } = require(`${basePath}/constants/blend_mode.js`);
+const { NETWORK } = require(`${basePath}/constants/network.js`);
+
+const network = NETWORK.eth;
+
+// General metadata for Ethereum
+const namePrefix = "RUNTS";
+const description = "RUNTS NFT COLLECTION";
+const baseUri = "ipfs://NewUriToReplace";
+
+const solanaMetadata = {
+  symbol: "YC",
+  seller_fee_basis_points: 1000, // Define how much % you want from secondary market sales 1000 = 10%
+  external_url: "https://www.youtube.com/c/hashlipsnft",
+  creators: [
+    {
+      address: "7fXNuer5sbZtaTEPhtJ5g5gNtuyRoKkvxdjEjEnPN4mC",
+      share: 100,
+    },
+  ],
+};
+
+// If you have selected Solana then the collection starts from 0 automatically
+const layerConfigurations = [
+   // Creates up to 50 artworks
+ {  growEditionSizeTo: 60,
+   layersOrder: [
+     { name: "backgrounds" },
+     { name: "alligator-fur" },
+     { name: "alligator-tears" },
+     { name: "alligator-aspirations" },
+   ],
+ },
+//  {
+//    // Creates an additional 100 artworks
+//    growEditionSizeTo: 101,
+//    layersOrder: [
+//      { name: "backgrounds" },
+//      { name: "axolotl-fur" },
+//      { name: "axolotl-tears" },
+//      { name: "axolotl-tattoo" },
+//      { name: "axolotl-aspirations" },
+//    ],
+//  },
+ {
+  // Creates an additional 100 artworks
+  growEditionSizeTo: 160,
+  layersOrder: [
+    { name: "backgrounds" },
+    { name: "bear-fur" },
+    { name: "bear-tears" },
+    { name: "bear-tattoo" },
+    { name: "bear-aspirations" },
+  ],
+},
+{
+  // Creates an additional 100 artworks
+  growEditionSizeTo: 140,
+  layersOrder: [
+    { name: "backgrounds" },
+    { name: "bull-fur" },
+    { name: "bull-tears" },
+    { name: "bull-tattoo" },
+    { name: "bull-aspirations" },
+  ],
+},
+{
+  // Creates an additional 100 artworks
+  growEditionSizeTo: 190,
+  layersOrder: [
+    { name: "backgrounds" },
+    { name: "cat-fur" },
+    { name: "cat-tears" },
+    { name: "cat-tattoo" },
+    { name: "cat-aspirations" },
+  ],
+},
+{
+  // Creates an additional 100 artworks
+  growEditionSizeTo: 210,
+  layersOrder: [
+    { name: "backgrounds" },
+    { name: "dog-fur" },
+    { name: "dog-tears" },
+    { name: "dog-tattoo" },
+    { name: "dog-aspirations" },
+  ],
+},
+{
+  // Creates an additional 100 artworks
+  growEditionSizeTo: 115,
+  layersOrder: [
+    { name: "backgrounds" },
+    { name: "puffer fish-fur" },
+    { name: "puffer fish-tears" },
+    { name: "puffer fish-tattoo" },
+    { name: "puffer fish-aspirations" },
+  ],
+},
+{
+  // Creates an additional 100 artworks
+  growEditionSizeTo: 135,
+  layersOrder: [
+    { name: "backgrounds" },
+    { name: "elephant-fur" },
+    { name: "elephant-tears" },
+    { name: "elephant-tattoo" },
+    { name: "elephant-aspirations" },
+  ],
+},
+];
+
+const shuffleLayerConfigurations = false;
+
+const debugLogs = false;
+
+const format = {
+  width: 512,
+  height: 512,
+  smoothing: false,
+};
+
+const gif = {
+  export: false,
+  repeat: 0,
+  quality: 100,
+  delay: 500,
+};
+
+const text = {
+  only: false,
+  color: "#ffffff",
+  size: 20,
+  xGap: 40,
+  yGap: 40,
+  align: "left",
+  baseline: "top",
+  weight: "regular",
+  family: "Courier",
+  spacer: " => ",
+};
+
+const pixelFormat = {
+  ratio: 2 / 128,
+};
+
+const backgrounds = {
+  generate: true,
+  brightness: "80%",
+  static: false,
+  default: "#000000",
+};
+
+const extraMetadata = {};
+
+const rarityDelimiter = "#";
+
+const uniqueDnaTorrance = 10000;
+
+const preview = {
+  thumbPerRow: 5,
+  thumbWidth: 50,
+  imageRatio: format.height / format.width,
+  imageName: "preview.png",
+};
+
+const preview_gif = {
+  numberOfImages: 5,
+  order: "ASC", // ASC, DESC, MIXED
+  repeat: 0,
+  quality: 100,
+  delay: 500,
+  imageName: "preview.gif",
+};
+
+module.exports = {
+  format,
+  baseUri,
+  description,
+  backgrounds,
+  uniqueDnaTorrance,
+  layerConfigurations,
+  rarityDelimiter,
+  preview,
+  shuffleLayerConfigurations,
+  debugLogs,
+  extraMetadata,
+  pixelFormat,
+  text,
+  namePrefix,
+  network,
+  solanaMetadata,
+  gif,
+  preview_gif,
+};
